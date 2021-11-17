dfc74b03429d384cc46c9d7fc572ac8fef326d27
Yanguo Cao <CaoY35@cardiff.ac.uk>
2021-11-17 16:29:19 +0000

    Created User profile model, repository and its service implementation. Also user profile controller. Closes #9

---
+++ src/main/java/com/community/client/requests/UserProfileRequest.java
@@ -0,0 +1,69 @@
+package com.community.client.requests;
+
+import javax.persistence.Column;
+
+public class UserProfileRequest {
+    private String name;
+    private String email;
+    private String aboutYourSelf;
+    private String qualifications;
+    private String hobbies;
+    private String achievements;
+    private int phone;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAboutYourSelf() {
+        return aboutYourSelf;
+    }
+
+    public void setAboutYourSelf(String aboutYourSelf) {
+        this.aboutYourSelf = aboutYourSelf;
+    }
+
+    public String getQualifications() {
+        return qualifications;
+    }
+
+    public void setQualifications(String qualifications) {
+        this.qualifications = qualifications;
+    }
+
+    public String getHobbies() {
+        return hobbies;
+    }
+
+    public void setHobbies(String hobbies) {
+        this.hobbies = hobbies;
+    }
+
+    public String getAchievements() {
+        return achievements;
+    }
+
+    public void setAchievements(String achievements) {
+        this.achievements = achievements;
+    }
+
+    public int getPhone() {
+        return phone;
+    }
+
+    public void setPhone(int phone) {
+        this.phone = phone;
+    }
+}
