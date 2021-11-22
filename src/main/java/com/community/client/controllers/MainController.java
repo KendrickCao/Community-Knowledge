8c3d48f84b59ca22a5a835eb75993a648ed52592
Madhur Jain <jainm10@cardiff.ac.uk>
2021-11-22 20:08:54 +0000

    HTML doc to display all communities while fetching real data from DB created. Link to open a detailed view of respective community established. Closes #24

--- src/main/java/com/community/client/controllers/MainController.java
+++ src/main/java/com/community/client/controllers/MainController.java
@@ -1,10 +1,21 @@
 package com.community.client.controllers;
 
+import com.community.client.models.Community;
+import com.community.client.services.CommunityService;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.util.Set;
+
 @RestController
 public class MainController {
+
+    //DI the community service
+    private CommunityService communityService;
+
+    public MainController(CommunityService communityService) {
+        this.communityService = communityService;
+    }
 
     @GetMapping("/SignUp")
     public ModelAndView showSignUpPage(ModelAndView modelAndView){
@@ -21,6 +32,15 @@ public class MainController {
     @RequestMapping("/add-community")
     public ModelAndView addCommunity(ModelAndView modelAndView){
         modelAndView.setViewName("create-community/index");
+        return modelAndView;
+    }
+
+    @RequestMapping("/communities")
+    public ModelAndView viewCommunities(ModelAndView modelAndView){
+        Set<Community> communities = communityService.getAllCommunities();
+        modelAndView.setViewName("community-listview/index");
+        modelAndView.addObject("communities", communities);
+
         return modelAndView;
     }
 }
