25800cdaf045a92707a8f510030f08b018baf5da
Elsa Merrett <MerrettEF@cardiff.ac.uk>
2021-11-22 10:44:37 +0000

    Add EventRepository to repositoris Ref #17

--- src/main/java/com/community/client/controllers/EventController.java
+++ src/main/java/com/community/client/controllers/EventController.java
@@ -1,4 +1,8 @@
 package com.community.client.controllers;
 
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class EventController {
+
 }
---
+++ src/main/java/com/community/client/services/EventService.java
@@ -0,0 +1,9 @@
+package com.community.client.services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class EventService {
+
+
+}
