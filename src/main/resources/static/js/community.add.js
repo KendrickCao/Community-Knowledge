3134bcd1787d7b31190f637407682a7b3ced2adf
Madhur Jain <jainm10@cardiff.ac.uk>
2021-11-29 10:06:24 +0000

    Logical check on window load has been added which redirects user to Login if user is not logged in. Closes #33

--- src/main/resources/static/js/community.add.js
+++ src/main/resources/static/js/community.add.js
@@ -4,8 +4,14 @@ let description;
 let communityImage;
 //We assume that the user is a logged in user cookie must be present
 let cookieArray =document.cookie.split(":")[2];
-let userId = cookieArray.split(",")[0];
+let userId = cookieArray?  cookieArray.split(",")[0]:null;
+window.onload = () =>{
 
+    if(!userId){
+        window.alert("You need to login first. Redirecting...")
+        window.location.href = "/Login"
+    }
+}
 
 //Define methods
 //Method to capture user input
