36ea728430e413b0560fff4291788bcbf490f6ab
Elsa Merrett <MerrettEF@cardiff.ac.uk>
2021-11-30 19:26:56 +0000

    Create Mockito test to check the get event by Id functionality works Ref #38

--- src/test/java/com/community/client/Event/EventTests.java
+++ src/test/java/com/community/client/Event/EventTests.java
@@ -33,8 +33,9 @@ public class EventTests {
         // Create community, project and event objects
         Community mockCommunity = new Community (1L);
         Project mockProject = new Project (1L);
-        Event mockEvent = new Event(1L,mockCommunity,mockProject,"20/12/2021","Winter Reads","CF14 4TY",
-               "Borrow winter themed books","Sandra Hail, John Smith","1", "image.jpg");
+        Event mockEvent = new Event(1L,mockCommunity,mockProject,"20/12/2021",
+                "Winter Reads","CF14 4TY", "Borrow winter themed books",
+                "Sandra Hail, John Smith","1", "image.jpg");
 
 
         // Mock the repository save method
@@ -55,6 +56,48 @@ public class EventTests {
         assertEquals("Sandra Hail, John Smith", savedEvent.getContributors());
         assertEquals("1", savedEvent.getCreatorUserId());
         assertEquals("image.jpg", savedEvent.getEventImage());
+    }
+
+    // Test to get event by its Id.
+    @Test
+    public void testToGetEventById(){
+
+        // Create mock event,community and project repositories
+        CommunityRepository mockCommunityRepository = mock(CommunityRepository.class);
+        ProjectRepository mockProjectRepository = mock(ProjectRepository.class);
+        EventRepository mockEventRepository = mock(EventRepository.class);
+
+        // Create Mock event, community and project services
+        CommunityService mockCommunityService = new CommunityService(mockCommunityRepository);
+        ProjectService mockProjectService = new ProjectService(mockProjectRepository);
+        EventService mockEventService = new EventService(mockEventRepository);
+
+        // Create community, project and event objects
+        Community mockCommunity = new Community (1L);
+        Project mockProject = new Project (1L);
+        Event mockEvent = new Event(1L, mockCommunity,mockProject,"20/12/2021",
+                "Winter Reads","CF14 4TY", "Borrow winter themed books",
+                "Sandra Hail, John Smith","1", "image.jpg");
+
+        // Mock the repository save method
+        when(mockEventRepository.findEventById(1L)).thenReturn(java.util.Optional.of(mockEvent));
+
+        // Save that event to the mockEventRepository
+        mockEventService.saveEvent(mockEvent);
+
+        // Get the event by Id
+        Event fetchedEvent = mockEventService.getEventById(1L);
+
+        // Make assertions
+        assertEquals(1, fetchedEvent.getCommunity().getId());
+        assertEquals(1, fetchedEvent.getProject().getId());
+        assertEquals("20/12/2021", fetchedEvent.getDate());
+        assertEquals("Winter Reads", fetchedEvent.getName());
+        assertEquals("CF14 4TY", fetchedEvent.getAddress());
+        assertEquals("Borrow winter themed books", fetchedEvent.getAboutSection());
+        assertEquals("Sandra Hail, John Smith", fetchedEvent.getContributors());
+        assertEquals("1", fetchedEvent.getCreatorUserId());
+        assertEquals("image.jpg", fetchedEvent.getEventImage());
     }
 
 }
