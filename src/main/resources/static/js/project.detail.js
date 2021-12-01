a3ec152b38ba348c67174711d596000874b10028
Yanguo Cao <CaoY35@cardiff.ac.uk>
2021-12-01 01:40:28 +0000

    Done the project detail page. Add community card so that user can link to the community which contain this project. Closes #27(1-3)

---
+++ src/main/resources/static/js/project.detail.js
@@ -0,0 +1,6 @@
+/*
+    var projectCoverUrl = "../uploads/" + [[${project.projectCoverImage}]];
+    console.log(projectCoverUrl);
+    var domProjectCover = document.getElementsByClassName("project-cover-bg")
+    domProjectCover.style.backgroundImage= "url('"+projectCoverUrl+"')";
+*/
