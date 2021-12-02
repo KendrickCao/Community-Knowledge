bfc3d1d1774f75bcb1214ab2bd713daed4fe82bf
Madhur(c21111089) <jainm10@cardiff.ac.uk>
2021-12-02 12:15:36 +0000

    Events and Projects now show dynamically from the DB on the commnity details page. Closes #41

--- src/main/java/com/community/client/controllers/MainController.java
+++ src/main/java/com/community/client/controllers/MainController.java
@@ -16,19 +16,20 @@ import java.util.Set;
 @RestController
 public class MainController {
 
-    //DI the community service
+    // DI the community service
     private CommunityService communityService;
 
-    //DI the event service
+    // DI the event service
     private EventService eventService;
 
-    //DI the user service
-    private UserObjectService  userObjectService;
+    // DI the user service
+    private UserObjectService userObjectService;
 
-    //DI project service
+    // DI project service
     private ProjectService projectService;
 
-    public MainController(CommunityService communityService, EventService eventService, UserObjectService userObjectService, ProjectService projectService) {
+    public MainController(CommunityService communityService, EventService eventService,
+            UserObjectService userObjectService, ProjectService projectService) {
         this.communityService = communityService;
         this.eventService = eventService;
         this.userObjectService = userObjectService;
@@ -40,11 +41,13 @@ public class MainController {
         modelAndView = new ModelAndView("/signup/SignUp");
         return modelAndView;
     }
+
     @GetMapping("/")
     public ModelAndView showLandingPage(ModelAndView modelAndView) {
         modelAndView = new ModelAndView("/landing/landing");
         return modelAndView;
     }
+
     @RequestMapping("/transaction")
     public ModelAndView showTransactionPage(ModelAndView modelAndView) {
         modelAndView = new ModelAndView("/transaction/transaction");
@@ -58,11 +61,10 @@ public class MainController {
     }
 
     @GetMapping("/UserProfile")
-    public ModelAndView showUserProfilePage(ModelAndView modelAndView){
+    public ModelAndView showUserProfilePage(ModelAndView modelAndView) {
         modelAndView = new ModelAndView("/userprofile/UserProfile");
         return modelAndView;
     }
-
 
     @GetMapping("/CreateProject")
     public ModelAndView showCreateProjectPage(ModelAndView modelAndView) {
@@ -77,37 +79,39 @@ public class MainController {
         return modelAndView;
     }
 
-    //Controller to load the VIEW which allows user to create a new community
+    // Controller to load the VIEW which allows user to create a new community
     @RequestMapping("/add-community")
     public ModelAndView addCommunity(ModelAndView modelAndView) {
         modelAndView.setViewName("create-community/index");
         return modelAndView;
     }
 
-
-    //Controller which allows the user the VIEW the details of a particular community
+    // Controller which allows the user the VIEW the details of a particular
+    // community
     @RequestMapping("/community/{communityId}")
     public ModelAndView viewCommunityDetails(ModelAndView modelAndView, @PathVariable Long communityId) {
-        //Get the community by ID
+        // Get the community by ID
         Community community = communityService.getCommunityById(communityId);
         Set<Project> projectSet = community.getProjectSet();
         Set<UserObject> userObjectSet = community.getUserObjectSet();
+        Set<Event> eventSet = community.getEvent();
 
         String totalUsers = String.valueOf(userObjectSet.size());
         String totalProjects = String.valueOf(projectSet.size());
-        System.out.println(totalUsers);
-        System.out.println(totalProjects);
+        String totalEvents = String.valueOf(eventSet.size());
         modelAndView.addObject("community", community);
         modelAndView.addObject("communityProjects", projectSet);
+        modelAndView.addObject("communityEvents", eventSet);
         modelAndView.addObject("communityUsers", userObjectSet);
-        modelAndView.addObject("communityUsersCount", totalUsers );
+        modelAndView.addObject("communityUsersCount", totalUsers);
         modelAndView.addObject("communityProjectsCount", totalProjects);
+        modelAndView.addObject("communityEventsCount", totalEvents);
 
         modelAndView.setViewName("community-detail/index");
         return modelAndView;
     }
 
-    //Controller which allows the user the VIEW to a list of all communities
+    // Controller which allows the user the VIEW to a list of all communities
     @RequestMapping("/communities")
     public ModelAndView viewCommunities(ModelAndView modelAndView) {
         Set<Community> communities = communityService.getAllCommunities();
@@ -116,13 +120,12 @@ public class MainController {
         return modelAndView;
     }
 
-    //Controller to view the events of a community/project
+    // Controller to view the events of a community/project
     @RequestMapping("/event/parent/{parentId}")
     public ModelAndView viewEventForCommunityOrProject(ModelAndView modelAndView,
-                                                       @RequestParam("parent") String parent,
-                                                       @PathVariable Long parentId
-    ) {
-        if (parent.equals("community")){
+            @RequestParam("parent") String parent,
+            @PathVariable Long parentId) {
+        if (parent.equals("community")) {
             Community community = communityService.getCommunityById(parentId);
             Set<Event> eventsFromParent = community.getEvent();
             modelAndView.addObject("eventsFromParent", eventsFromParent);
@@ -135,24 +138,22 @@ public class MainController {
         return modelAndView;
     }
 
-    //Controller which allows the user the VIEW the details of a particular event
+    // Controller which allows the user the VIEW the details of a particular event
     @RequestMapping("/event/{eventId}")
     public ModelAndView viewEventDetails(ModelAndView modelAndView, @PathVariable Long eventId) {
-        //Get the event by ID
+        // Get the event by ID
         Event event = eventService.getEventById(eventId);
         Community eventCommunity = event.getCommunity();
         Project eventProject = event.getProject();
         modelAndView.addObject("event", event);
-            modelAndView.addObject("eventCommunity",eventCommunity );
-            modelAndView.addObject("eventProject", eventProject);
+        modelAndView.addObject("eventCommunity", eventCommunity);
+        modelAndView.addObject("eventProject", eventProject);
         modelAndView.setViewName("event-detail/EventDetail");
         return modelAndView;
     }
 
-
-
-    //Controller which allows the user the VIEW to a list of all projects
-    @RequestMapping ("/projects")
+    // Controller which allows the user the VIEW to a list of all projects
+    @RequestMapping("/projects")
     public ModelAndView viewProjects(ModelAndView modelAndView) {
         Set<Project> projects = projectService.getAllProjects();
         modelAndView.setViewName("project/projects");
@@ -160,8 +161,8 @@ public class MainController {
         return modelAndView;
     }
 
-    @RequestMapping ("/project/{projectId}")
-    public ModelAndView viewProjects(ModelAndView modelAndView,@PathVariable Long projectId) {
+    @RequestMapping("/project/{projectId}")
+    public ModelAndView viewProjects(ModelAndView modelAndView, @PathVariable Long projectId) {
         Project project = projectService.getProjectById(projectId);
         Community projectCommunity = project.getCommunity();
         Set<Event> projectEvents = project.getEvent();
