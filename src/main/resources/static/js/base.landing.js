d97eb34a3589707d87f7aa8dc5b1d0b47e9e3481
tech@yosanie.com <jainm10@cardiff.ac.uk>
2021-12-06 19:59:22 +0000

    Form validations in place for add community. Used Regular expressions to check digits or special characters. Closes #53

--- src/main/resources/static/js/base.landing.js
+++ src/main/resources/static/js/base.landing.js
@@ -1,54 +1,54 @@
-// On load fetch the communities and projects.
-window.onload = async () => {
-  // Get the elements by class name.
-  let communityNameContainerElement = document.getElementsByClassName(
-    "CompanyCurrentNewsHeadlines"
-  )[0];
-  let communityImageContainerElement = document.getElementsByClassName(
-    "companyBannerSubContent"
-  )[0];
-  let projectNameContainerElement = document.getElementsByClassName(
-      "projectNameContainer"
-  )[0];
-  let projectImageContainerElement = document.getElementsByClassName(
-      "projectImageContainer"
-  )[0];
+// // On load fetch the communities and projects.
+// window.onload = async () => {
+//   // Get the elements by class name.
+//   let communityNameContainerElement = document.getElementsByClassName(
+//     "CompanyCurrentNewsHeadlines"
+//   )[0];
+//   let communityImageContainerElement = document.getElementsByClassName(
+//     "companyBannerSubContent"
+//   )[0];
+//   let projectNameContainerElement = document.getElementsByClassName(
+//       "projectNameContainer"
+//   )[0];
+//   let projectImageContainerElement = document.getElementsByClassName(
+//       "projectImageContainer"
+//   )[0];
 
-  const responseCommunity = await fetch("http://localhost:8081/api/communities");
-  const dataCommunity = await responseCommunity.json()
-  const maxLengthCommunity = dataCommunity.length < 4 ? dataCommunity.length :4 ;
-  const responseProject = await fetch("http://localhost:8081/api/projects");
-  const dataProject = await responseProject.json();
-  const maxLengthEvent = dataProject.length < 4 ? dataProject.length :4 ;
+//   const responseCommunity = await fetch("http://localhost:8081/api/communities");
+//   const dataCommunity = await responseCommunity.json()
+//   const maxLengthCommunity = dataCommunity.length < 4 ? dataCommunity.length :4 ;
+//   const responseProject = await fetch("http://localhost:8081/api/projects");
+//   const dataProject = await responseProject.json();
+//   const maxLengthEvent = dataProject.length < 4 ? dataProject.length :4 ;
 
-  if (dataCommunity) {
-    for (let i = 0; i < maxLengthCommunity; i++) {
-      const a = document.createElement("a");
-      a.innerText = dataCommunity[i].name;
-      a.setAttribute("href", `http://localhost:8081/community/${dataCommunity[i].id}`);
-      communityNameContainerElement.append(a);
-      // Upload images
-      const div = document.createElement("div");
-      div.classList.add("importantTopic");
-      const sourceString = `http://localhost:8081/uploads/${dataCommunity[i].communityImage}`;
-      div.innerHTML = `<img style="{{object-fit: contain}}"   src=${sourceString} alt="Community image">`;
-      communityImageContainerElement.append(div);
-    }
-  }
+//   if (dataCommunity) {
+//     for (let i = 0; i < maxLengthCommunity; i++) {
+//       const a = document.createElement("a");
+//       a.innerText = dataCommunity[i].name;
+//       a.setAttribute("href", `http://localhost:8081/community/${dataCommunity[i].id}`);
+//       communityNameContainerElement.append(a);
+//       // Upload images
+//       const div = document.createElement("div");
+//       div.classList.add("importantTopic");
+//       const sourceString = `http://localhost:8081/uploads/${dataCommunity[i].communityImage}`;
+//       div.innerHTML = `<img style="{{object-fit: contain}}"   src=${sourceString} alt="Community image">`;
+//       communityImageContainerElement.append(div);
+//     }
+//   }
 
-  if (dataProject) {
-    for (let i = 0; i < maxLengthEvent; i++) {
-      const a = document.createElement("a");
-      a.innerText = dataProject[i].name;
-      a.setAttribute("href", `http://localhost:8081/project/${dataProject[i].id}`);
-      projectNameContainerElement.append(a);
-      // Upload images
-      const div = document.createElement("div");
-      div.classList.add("importantTopicProject");
-      const sourceString = `http://localhost:8081/uploads/${dataProject[i].projectCoverImage}`;
-      div.innerHTML = `<img style="{{object-fit: contain}}"   src=${sourceString} alt="Project image">`;
-      projectImageContainerElement.append(div);
-    }
-  }
+//   if (dataProject) {
+//     for (let i = 0; i < maxLengthEvent; i++) {
+//       const a = document.createElement("a");
+//       a.innerText = dataProject[i].name;
+//       a.setAttribute("href", `http://localhost:8081/project/${dataProject[i].id}`);
+//       projectNameContainerElement.append(a);
+//       // Upload images
+//       const div = document.createElement("div");
+//       div.classList.add("importantTopicProject");
+//       const sourceString = `http://localhost:8081/uploads/${dataProject[i].projectCoverImage}`;
+//       div.innerHTML = `<img style="{{object-fit: contain}}"   src=${sourceString} alt="Project image">`;
+//       projectImageContainerElement.append(div);
+//     }
+//   }
 
-};
+// };
