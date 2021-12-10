ebf16a43fdae471a6128aadb41bfa2894b5c403d
Yanguo Cao <CaoY35@cardiff.ac.uk>
2021-12-10 03:23:34 +0000

    Write js to control the progress bar automatically change.Close #61

--- src/main/resources/static/js/project.detail.js
+++ src/main/resources/static/js/project.detail.js
@@ -1,6 +1,9 @@
-/*
-    var projectCoverUrl = "../uploads/" + [[${project.projectCoverImage}]];
-    console.log(projectCoverUrl);
-    var domProjectCover = document.getElementsByClassName("project-cover-bg")
-    domProjectCover.style.backgroundImage= "url('"+projectCoverUrl+"')";
-*/
+// Get Dom Elements
+let progressBarElement = document.getElementById("progressbar");
+let fundsRequiredElement = document.getElementById("fundsRequired").innerText;
+let fundsCollectedElement = document.getElementById("fundsCollected").innerText;
+fundsRequired = fundsRequiredElement.substring(15,fundsRequiredElement.length - 7);
+fundsCollected =fundsCollectedElement.substring(16,fundsCollectedElement.length - 7);
+let progressNum = (fundsCollected/fundsRequired).toFixed(4)*100
+progressBarElement.style.width = progressNum +'%'
+console.log(progressNum);
