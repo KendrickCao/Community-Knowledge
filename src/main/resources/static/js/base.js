58b3275d7bbec69d0aeff56b71b65fd3ac108d4e
Yanguo Cao <CaoY35@cardiff.ac.uk>
2021-12-11 21:58:54 +0000

    Change arrow css on header

--- src/main/resources/static/js/base.js
+++ src/main/resources/static/js/base.js
@@ -7,10 +7,11 @@ const btnTimes = document.querySelector('.times-btn');
 const navBar = document.getElementById('nav-bar');
 
 // Code for show username when user login
+let accountNameElement = document.getElementById("accountName");
 let cookieArrayForLog =document.cookie.split(":")[3]
 let userName = cookieArrayForLog ? cookieArrayForLog.split("\"")[1]:null;
 if (userName!=null) {
- window.alert(userName)
+    accountNameElement.innerText = userName;
 }
 // Creating the EventListener for the btnHam
 btnHam.addEventListener('click', function (){
