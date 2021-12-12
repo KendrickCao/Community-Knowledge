e47fd2e7e95b00f07ee025ec938442b31738baa8
Yanguo Cao <CaoY35@cardiff.ac.uk>
2021-12-12 00:43:28 +0000

    Done the logout function. Close #59

--- src/main/resources/static/js/base.js
+++ src/main/resources/static/js/base.js
@@ -6,12 +6,28 @@ const btnHam = document.querySelector('.ham-btn');
 const btnTimes = document.querySelector('.times-btn');
 const navBar = document.getElementById('nav-bar');
 
-// Code for show username when user login
+// Code for show username and logout when user login
 let accountNameElement = document.getElementById("accountName");
 let cookieArrayForLog =document.cookie.split(":")[3]
 let userName = cookieArrayForLog ? cookieArrayForLog.split("\"")[1]:null;
+let loginElement = document.getElementById("fa-user-login");
+let logoutElement = document.getElementById("fa-user-logout")
 if (userName!=null) {
     accountNameElement.innerText = userName;
+    loginElement.style.display = "none";
+    logoutElement.style.display = "inline";
+}
+//logout function
+logoutElement.onclick = function (){
+    //clear all cookie
+    let keys = document.cookie.match(/[^ =;]+(?=\=)/g);
+    if(keys) {
+        for(let i = keys.length; i--;){
+            document.cookie = keys[i] + '=0;expires=' + new Date(0).toUTCString()
+        }
+    }
+    //refresh this page
+    location.reload();
 }
 // Creating the EventListener for the btnHam
 btnHam.addEventListener('click', function (){
