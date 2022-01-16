5a4e481f5c924a479f56da610fdf932a364fdd56
Chao Li <lic85@cardiff.ac.uk>
2022-01-16 14:29:24 +0000

    Update AddressService.java

--- src/main/java/com/community/client/services/AddressService.java
+++ src/main/java/com/community/client/services/AddressService.java
@@ -7,18 +7,23 @@ import org.springframework.stereotype.Service;
 
 import java.util.Optional;
 
+//Address service layer
 @Service
 public class AddressService {
     public AddressRepository addressRepository;
 
+    //constructor
     public AddressService(AddressRepository addressRepository) {
         this.addressRepository = addressRepository;
     }
 
+    // Implement the methods from repository,and it will be used in controller layer
+    //implement the save address
     public Address saveAddress(Address address) {
         return addressRepository.save(address);
     }
 
+    //implement the get an address by id
     public Address getAddressById(Long id){
         Optional<Address> addressOptional = addressRepository.findAddressById(id);
         if (addressOptional.isPresent()){
