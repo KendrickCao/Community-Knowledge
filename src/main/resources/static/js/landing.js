beff05546975ab666e9e381ae99cfbee22b6023b
Yanguo Cao <caoy35@cardiff.ac.uk>
2022-03-03 15:45:33 +0000

    Update landing.js

--- src/main/resources/static/js/landing.js
+++ src/main/resources/static/js/landing.js
@@ -2,7 +2,7 @@
 const fetchAll = async () => {
   //Fetch the community Data
   const responseCommunity = await fetch(
-    "http://localhost:8081/api/communities"
+    "/api/communities"
   );
 
   const dataCommunity = await responseCommunity.json();
@@ -12,14 +12,14 @@ const fetchAll = async () => {
     dataCommunity.length < 4 ? dataCommunity.length : 4;
 
   //Fetch the Projects data
-  const responseProject = await fetch("http://localhost:8081/api/projects");
+  const responseProject = await fetch("/api/projects");
   const dataProject = await responseProject.json();
 
   //Restrict Project to 4
   const maxLengthProject = dataProject.length < 4 ? dataProject.length : 4;
 
   //Fetch events data
-  const responseEvent = await fetch("http://localhost:8081/api/events");
+  const responseEvent = await fetch("/api/events");
   const dataEvent = await responseEvent.json();
 
   //Restrict events to 4
@@ -32,13 +32,13 @@ const fetchAll = async () => {
       a.innerText = dataCommunity[i].name;
       a.setAttribute(
         "href",
-        `http://localhost:8081/community/${dataCommunity[i].id}`
+        `/community/${dataCommunity[i].id}`
       );
 
       // Upload images
       const div = document.createElement("div");
       div.classList.add("community_card");
-      const sourceString = `http://localhost:8081/uploads/${dataCommunity[i].communityImage}`;
+      const sourceString = `/uploads/${dataCommunity[i].communityImage}`;
       div.innerHTML = `<img src=${sourceString} alt="Community image">`;
       div.append(a);
       document.getElementById("community_card--container").append(div);
@@ -51,12 +51,12 @@ const fetchAll = async () => {
       a.innerText = dataProject[i].name;
       a.setAttribute(
         "href",
-        `http://localhost:8081/project/${dataProject[i].id}`
+        `/project/${dataProject[i].id}`
       );
       // Upload images
       const div = document.createElement("div");
       div.classList.add("community_card");
-      const sourceString = `http://localhost:8081/uploads/${dataProject[i].projectCoverImage}`;
+      const sourceString = `/uploads/${dataProject[i].projectCoverImage}`;
       div.innerHTML = `<img src=${sourceString} alt="Project image">`;
       div.append(a);
       document.getElementById("project_card--container").append(div);
@@ -67,11 +67,11 @@ const fetchAll = async () => {
     for (let i = 0; i < maxLengthEvent; i++) {
       const a = document.createElement("a");
       a.innerText = dataEvent[i].name;
-      a.setAttribute("href", `http://localhost:8081/event/${dataEvent[i].id}`);
+      a.setAttribute("href", `/event/${dataEvent[i].id}`);
       // Upload images
       const div = document.createElement("div");
       div.classList.add("community_card");
-      const sourceString = `http://localhost:8081/uploads/${dataEvent[i].eventImage}`;
+      const sourceString = `/uploads/${dataEvent[i].eventImage}`;
       div.innerHTML = `<img  src=${sourceString} alt="Event image">`;
       div.append(a);
       document.getElementById("event_card--container").append(div);
